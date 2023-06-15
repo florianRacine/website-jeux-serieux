63bbb0991b94a9e5fadca11c6d2efeb1f9466932
Florian Racine <103432737+florianRacine@users.noreply.github.com>
2023-06-15 16:16:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const helmet = require("helmet");
 
 // ======= variables =======
@@ -21,55 +21,24 @@ let adminIncoming = false;
 // ============= SERVER =============
 // ==================================
 
-// const Server = http.createServer(app)
-// Server.listen(process.env.LEANPORT, () => {
-//   console.log(`Lean Management runs on ${process.env.LEANPORT}'s port`);
-// });
-
-// ======= Lean Management server =======
-const leanServer = http.createServer(app)
-const ioLean = require('./ioServers/LeanManagement');
-ioLean(leanServer);
-leanServer.listen(process.env.LEANPORT, () => {
-  console.log(`Lean Management runs on ${process.env.LEANPORT}'s port`);
-});
-
-// ======= Extreme Evolution server =======
-const serverExtreme = http.createServer(app)
-const ioForEE = require('./ioServers/ExtremeEvolution');
-ioForEE(serverExtreme);
-serverExtreme.listen(process.env.EXTREMEPORT, () => {
-  console.log(`Extreme Evolution runs on ${process.env.EXTREMEPORT}'s port`);
-});
-
-// ======= Frenetic Maintenance server =======
-const freneticServer = http.createServer(app)
-const ioFrenetic = require('./ioServers/FreneticMaintenance');
-ioFrenetic(freneticServer);
-freneticServer.listen(process.env.FRENETICPORT, () => {
-  console.log(`Frenetic Maintenance runs on ${process.env.FRENETICPORT}'s port`);
-});
-
-// ======= Frenetic Maintenance server =======
-const guardiansServer = http.createServer(app)
-const ioGuardians = require('./ioServers/theGuardians');
-ioGuardians(guardiansServer);
-guardiansServer.listen(process.env.GUARDIANSPORT, () => {
-  console.log(`The Guardians runs on ${process.env.GUARDIANSPORT}'s port`);
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // =====================================
 // ============ APP SETTINGS ===========
 // =====================================
 
-app.use(helmet()); 
+app.use(helmet());
 
-app.use(sessionMiddleware); // server use session
+app.use(sessionMiddleware);
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // server can use json
+app.use(express.json());
 
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
   const { idUtilisateur } = req.session;
   if (idUtilisateur) {
     res.locals.utilisateur = utilisateurs.find(
@@ -79,11 +48,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "public"))); // set the static path
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine", "ejs"); // set the type of views
-app.set("views", path.join(__dirname, "views")); // path of all views
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // ===========================================
 // ============ INSCRIPTION ROADS ============
@@ -91,69 +59,78 @@ app.set("views", path.join(__dirname, "views")); // path of all views
 
 app.get("/", (req, res) => {
   const { utilisateur } = res.locals;
-  res.render("index", { games, utilisateur, errorNameMessage, adminIncoming, errorPasswordMessage});
+  res.render("index", {
+    games,
+    utilisateur,
+    errorNameMessage,
+    adminIncoming,
+    errorPasswordMessage,
+  });
 });
 
 app.post("/inscription", (req, res) => {
   const { firstname } = req.body;
   errorNameMessage = "";
 
-  if (firstname){
+  if (firstname) {
     let playerExist = false;
-    if (firstname !== "admin") { // possible to have severals administrators
-      playerExist = (utilisateurs.some(
-        (user) => user.firstname === firstname
-      ));
+    if (firstname !== "admin") {
+      playerExist = utilisateurs.some((user) => user.firstname === firstname);
     } else {
       adminIncoming = true;
       return res.redirect("/");
     }
 
-    if (!playerExist){
-      if (isValidUserName(firstname)){
+    if (!playerExist) {
+      if (isValidUserName(firstname)) {
         let nouvelUtilisateur = {
-          "id": uuidv4(),
-          "firstname": firstname,
-          "game": {}
+          id: uuidv4(),
+          firstname: firstname,
+          game: {},
         };
         utilisateurs.push(nouvelUtilisateur);
         req.session.idUtilisateur = nouvelUtilisateur.id;
         console.log(`Welcome ${firstname}`);
         return res.redirect("/");
       }
-    } else { 
-      errorNameMessage = `${firstname} is already used`
+    } else {
+      errorNameMessage = `${firstname} is already used`;
     }
-  } else{
-    errorNameMessage = "The field is empty"
+  } else {
+    errorNameMessage = "The field is empty";
   }
 
   const { utilisateur } = res.locals;
-  res.render("index", {games, utilisateur, errorNameMessage, adminIncoming})
+  res.render("index", {
+    games,
+    utilisateur,
+    errorNameMessage,
+    adminIncoming,
+  });
 });
 
 function isValidUserName(nameToTest) {
-  if (!/^[a-zA-Z0-9]+$/.test(nameToTest)) {  
-    errorNameMessage = 'You are using wrong caracters'
+  if (!/^[a-zA-Z0-9]+$/.test(nameToTest)) {
+    errorNameMessage = "You are using wrong characters";
     return false;
   }
 
   if (nameToTest.length > 8) {
-    errorNameMessage = 'This name is too long'
+    errorNameMessage = "This name is too long";
     return false;
   }
-  
+
   return true;
 }
 
 app.post("/verifpassword", (req, res) => {
   const { password } = req.body;
 
-  if (password === process.env.ADMPSW){ // password's good 
+  if (password === process.env.ADMPSW) {
     let nouvelUtilisateur = {
-      "id": uuidv4(),
-      "firstname": "admin",
-      "game": {}
+      id: uuidv4(),
+      firstname: "admin",
+      game: {},
     };
     utilisateurs.push(nouvelUtilisateur);
     req.session.idUtilisateur = nouvelUtilisateur.id;
@@ -161,16 +138,15 @@ app.post("/verifpassword", (req, res) => {
     adminIncoming = false;
     errorPasswordMessage = "";
     return res.redirect("/");
-
-  } else { 
-    errorPasswordMessage = "Wrong password"
+  } else {
+    errorPasswordMessage = "Wrong password";
     return res.redirect("/");
   }
 });
 
 app.get("/deconnexion", (req, res) => {
   adminIncoming = false;
-  // delete the player in the json
+
   const { idUtilisateur } = req.session;
   const index = utilisateurs.findIndex(
     (utilisateur) => utilisateur.id === idUtilisateur
@@ -180,7 +156,6 @@ app.get("/deconnexion", (req, res) => {
     utilisateurs.splice(index, 1);
   }
 
-  // delete the session 
   req.session.destroy((err) => {
     if (err) {
       return res.redirect("/");
@@ -190,21 +165,20 @@ app.get("/deconnexion", (req, res) => {
   });
 });
 
-
 // ====================================
 // ============ GAME ROADS ============
 // ====================================
 
 const protectionRoute = (req, res, next) => {
   if (!req.session.idUtilisateur) {
-    errorNameMessage = "You must enter a name"
-    res.redirect("/")
+    errorNameMessage = "You must enter a name";
+    res.redirect("/");
   } else {
     next();
   }
 };
 
-// ======= Lean Management ======= 
+// ======= Lean Management =======
 app.get("/leanManagement", protectionRoute, (req, res) => {
   const { utilisateur } = res.locals;
   res.render("games/leanManagement", { utilisateur });
@@ -216,7 +190,7 @@ app.get("/extremeEvolution", protectionRoute, (req, res) => {
   res.render("games/extremeEvolution", { utilisateur });
 });
 
-// ======= Frentetique maintenance =======
+// ======= Frenetic Maintenance =======
 app.get("/freneticMaintenance", protectionRoute, (req, res) => {
   const { utilisateur } = res.locals;
   res.render("games/freneticMaintenance", { utilisateur });
@@ -227,3 +201,4 @@ app.get("/theGuardians", protectionRoute, (req, res) => {
   const { utilisateur } = res.locals;
   res.render("games/theGuardians", { utilisateur });
 });
+
